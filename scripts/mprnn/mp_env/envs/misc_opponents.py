7f2cd7372a315b79f03ed9bb07fbaa4b016f11b3
SRodwin <95655948+SRodwin@users.noreply.github.com>
2023-04-20 21:36:16 -0400

    Update misc_opponents.py

--- scripts/mprnn/mp_env/envs/misc_opponents.py
+++ scripts/mprnn/mp_env/envs/misc_opponents.py
@@ -184,7 +184,7 @@ class PatternBandit(Opponent):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
 
-        self.pattern = [random.randint(0, 1) for i in range(2)] #fixed pattern to play
+        self.pattern = [random.randint(0, 1) for i in range(4)] #fixed pattern to play
         self.plen = len(self.pattern)
         self.count = 0
         self.max_choices = []
